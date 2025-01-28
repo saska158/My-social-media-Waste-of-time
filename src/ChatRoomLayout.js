2333bfa83f2331cced39e98b6ef72aed70f3159c
Saska <mikic.saska@gmail.com>
2025-01-28 16:02:43 +0100

    updates

--- src/ChatRoomLayout.js
+++ src/ChatRoomLayout.js
@@ -1,10 +1,10 @@
-import { useState, useEffect, useMemo } from "react"
-import { NavLink, Outlet, useParams, useNavigate } from "react-router-dom"
-import { database, ref, push, onValue } from "./firebase"
-import { useAuth } from './authContext'
+//import { useState, useEffect, useMemo } from "react"
+import { NavLink, Outlet/*, useParams, useNavigate */} from "react-router-dom"
+////import { database, ref, push, onValue } from "./firebase"
+//import { useAuth } from './authContext'
 //import Form from "./Form"
-import Input from "./Input"
-import Button from "./Button"
+//import Input from "./Input"
+//import Button from "./Button"
 
 /*
 funkcija send message treba da proveri da li je user signinovan, 
